db81df0417fa8b36ee687e7676b5588fe1294aca
Irina Keil <35578733+Luftschutzkeller@users.noreply.github.com>
2021-10-24 20:39:55 +0300

    Stage 3

--- Coffee Machine/task/src/machine/Main.kt
+++ Coffee Machine/task/src/machine/Main.kt
@@ -1,10 +1,33 @@
 package machine
 
 fun main() {
+
+    val water = readLine()!!.toInt()
+    println("Write how many ml of water the coffee machine has: $water")
+    val milk = readLine()!!.toInt()
+    println("Write how many ml of milk the coffee machine has: $milk")
+    val beans = readLine()!!.toInt()
+    println("Write how many grams of coffee beans the coffee machine has: $beans")
     val cups = readLine()!!.toInt()
     println("Write how many cups of coffee you will need: $cups")
-    println("For $cups cups of coffee you will need:")
-    println("${cups * 200} ml of water")
-    println("${cups * 50} ml of milk")
-    println("${cups * 15} g of coffee beans")
+    val a = water / 200
+    val b = milk / 50
+    val c = beans / 15
+    var amount = 0
+    amount += if ((b in c..a) || (a in c..b)) {
+        c
+    } else if ((c in b..a) || (a in b..c) ){
+        b
+    } else{
+        a
+    }
+
+    val min: Int = if (a <= b && a <= c) {a} else if (b <= c && b <= a) {b} else {c}
+    if (amount == cups) {
+        println("Yes, I can make that amount of coffee")
+    } else if (amount > cups) {
+        println("Yes, I can make that amount of coffee (and even ${amount - cups} more than that)")
+    } else if (amount < cups) {
+        println("No, I can make only $min cups of coffee")
+    }
 }
